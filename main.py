27edc91cd932ee20907a0c7d6d1b79bdf9f106e8
JakaIpaLak <143828983+JakaIpaLak@users.noreply.github.com>
2023-09-01 21:55:24 +0200

    Končna verzija

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if os.path.isfile("Bloki.txt"): #Preveri če besedilo že obstaja
     while True:
         response = input("Ali želiš ponovno zajeti spletno stran?  Y/N  ")
         if response.lower() not in ["y", "n"]:
-            print("Vnesi Y za ja oz. N na ne")
+            print("Vnesi Y za ja oz. N za ne")
             continue
         else:
             if response.lower() == "y": preberi = True
@@ -30,7 +30,7 @@ if preberi == True:
     while True: #shrani besedilo
         response = input("Ali želiš shraniti zajeto stran in pridobljene bloke besedila?  Y/N  ")
         if response.lower() not in ["y", "n"]:
-            print("Vnesi Y za ja oz. N na ne")
+            print("Vnesi Y za ja oz. N za ne")
             continue
         else:
             if response.lower() == "y": 
@@ -46,21 +46,21 @@ d1=Podatki()
 
 #regex vzorci
 pattern_dict={
-    "Aircraft id" : r'<td><a href="ent_show.php\?ent=4&amp;AT_ID=(.+?)"',
-    "Aircraft name" : r'<td class="mainname"><a href="ent_show.php\?ent=4&amp;AT_ID=.+?">(.+?)</a></td>',
-    "Manufacturer" : r'<a href="ent_show.php\?ent=3&amp;MAN_ID=.+?">(.+?)</a></td>',
-    "Country" : r'<a href="ent_list.php\?ent=4&amp;MAN_Country=.+?">(.+?)</a></td>',
+    "Letalo_ID" : r'<td><a href="ent_show.php\?ent=4&amp;AT_ID=(.+?)"',
+    "Ime_letala" : r'<td class="mainname"><a href="ent_show.php\?ent=4&amp;AT_ID=.+?">(.+?)</a></td>',
+    "Izdelovalec" : r'<a href="ent_show.php\?ent=3&amp;MAN_ID=.+?">(.+?)</a></td>',
+    "Drzava_porekla" : r'<a href="ent_list.php\?ent=4&amp;MAN_Country=.+?">(.+?)</a></td>',
 }
 pattern_other=re.compile(
-    r'<td class="center">(?:|.+?">(?P<Category>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<Role>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<Engine_type>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<Engines>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Kategorija>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Vloga>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Tip_motorja>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Stevilo_motorjev>.*?)<.+?)</td>.*?'
     r'<td class="center">(?:|.+?">(?P<WTC>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<Seats>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<First_flight>.*?)<.+?)</td>.*?'
-    r'<td class="center">(?:|.+?">(?P<Last_manufactured>.*?)<.+?)</td>.*?'
-    r'<td class="right">(?:|(?P<Number_built>.+?))</td>',
+    r'<td class="center">(?:|.+?">(?P<Stevilo_sedezev>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Leto_prvega_leta>.*?)<.+?)</td>.*?'
+    r'<td class="center">(?:|.+?">(?P<Leto_zadnje_izdelave>.*?)<.+?)</td>.*?'
+    r'<td class="right">(?:|(?P<Stevilo_zgrajenih>.+?))</td>',
     re.DOTALL
 )
 
@@ -69,5 +69,5 @@ for i in pattern_dict:
     d1.find_data(html_blocks, re.compile(pattern_dict[i]), i)
 d1.find_data_no_dict(html_blocks, pattern_other)
 
-#shrni podatke
+#shrani podatke
 d1.save_data("Podatki.csv")
